8318328e867b99bea3fd97e487f4bac1033277f7
gabriele131099 <gabrisanna131099@gmail.com>
2021-05-27 14:37:39 +0200

    struttura

---
+++ js/script.js
@@ -0,0 +1,6 @@
+alert("Generatore di password");
+var Nome = prompt("Nome?");
+var Cognome = prompt("Cognome?");
+var colorePreferito = prompt("Colore Preferito?");
+var a = 21;
+document.getElementById('risultato').innerHTML= Nome + Cognome + colorePreferito + a;
