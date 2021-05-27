d567b93caf940cc5957dd1fc85c7a9274692f111
gabriele131099 <gabrisanna131099@gmail.com>
2021-05-27 17:28:21 +0200

    struttura

--- bonus/script.js
+++ bonus/script.js
@@ -2,5 +2,5 @@ function myFunction() {
     var nome = document.getElementById("nome").value;
     var cognome = document.getElementById("cognome").value;
     var colore = document.getElementById("colore").value;
-    document.getElementById("password").innerHTML = nome + cognome + colore + dataNascita;
+    document.getElementById("password").innerHTML = nome + cognome + colore;
 }
